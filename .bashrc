50e8a319fac9e7bdc0eb5b4c4258987b05f013ac
lindy2076 <kosolapov.agent@yandex.ru>
2023-11-11 19:46:15 +0300

    glow config added

--- .bashrc
+++ .bashrc
@@ -117,3 +117,4 @@ if ! shopt -oq posix; then
 fi
 
 alias anaconda='source ~/.conda.bashrc'
+export EDITOR=vim
--- dotconfig.sh
+++ dotconfig.sh
@@ -3,15 +3,15 @@
 I3_CONFIG_DIR="${HOME}/.config/i3/"
 I3_STATUS_CONFIG_DIR="${HOME}/.config/i3status/"
 KITTY_DIR="${HOME}/.config/kitty/"
+GLOW_DIR="${HOME}/.config/glow"
 BASH_DIR=${HOME}"/.bashrc"
 VIM_DIR="${HOME}/.vimrc"
 
-DIRS=($I3_CONFIG_DIR $I3_STATUS_CONFIG_DIR $KITTY_DIR)
+DIRS=($I3_CONFIG_DIR $I3_STATUS_CONFIG_DIR $KITTY_DIR $GLOW_DIR)
 FILES=($BASH_DIR $VIM_DIR)
 
 for d in "${DIRS[@]}"
 do
-	echo $d
 	cp -r "$d" "${HOME}/dotconfigs"
 done
 
